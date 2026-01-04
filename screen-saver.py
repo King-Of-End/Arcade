3b45c83ed95fea1f2c7102a9a2e178fa1d26e7fd
KramK41378 <karafizimark@yandex.ru>
2026-01-04 13:30:33 +0300

    Update screen-saver.py
    добавил задний фон

--- screen-saver.py
+++ screen-saver.py
@@ -18,9 +18,15 @@ class ScreenSaverView(arcade.View):
                                      arcade.color.WHITE, font_size=40, anchor_x="center", batch=self.batch)
         self.space_text = arcade.Text("Нажми SPACE, чтобы начать!", self.window.width / 2, self.window.height / 2 - 50,
                                       arcade.color.WHITE, font_size=20, anchor_x="center", batch=self.batch)
+        self.background = arcade.load_texture("background.jpg")
 
     def on_draw(self):
         self.clear()
+        arcade.draw_texture_rect(self.background, arcade.Rect(x=self.width // 2, y=self.height // 2,
+                                                              width=self.width, height=self.height, bottom=0, left=0,
+                                                              right=self.width, top=self.height),
+                                 color=arcade.color.WHITE, angle=0.0, blend=True,
+                                 alpha=255, pixelated=False, atlas=None)
         self.batch.draw()
 
     def on_key_press(self, key, modifiers):
@@ -45,7 +51,8 @@ class MenuView(arcade.View):
 
         self.anchor_layout.add(self.box_layout)  # Box в anchor
         self.manager.add(self.anchor_layout)
-        # Здесь сама игра
+        
+        self.background = arcade.load_texture("background.jpg")
 
     def setup_widgets(self):
         change_person_button = UIFlatButton(text="Выбрать персонажа", width=400, height=80, color=arcade.color.BLUE)
@@ -61,6 +68,11 @@ class MenuView(arcade.View):
 
     def on_draw(self):
         self.clear()
+        arcade.draw_texture_rect(self.background, arcade.Rect(x=self.width // 2, y=self.height // 2,
+                                                              width=self.width, height=self.height, bottom=0, left=0,
+                                                              right=self.width, top=self.height),
+                                 color=arcade.color.WHITE, angle=0.0, blend=True,
+                                 alpha=255, pixelated=False, atlas=None)
         self.manager.draw()
         # Рисуем спрайты, сцену...
 
