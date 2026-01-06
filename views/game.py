8d64d9ebda5a73c12f16028ba41622f23562417e
KramK41378 <karafizimark@yandex.ru>
2026-01-06 18:24:07 +0300

    начал работу над частицами

--- views/game.py
+++ views/game.py
@@ -6,12 +6,36 @@ from typing import Tuple, List
 
 import arcade
 from arcade import SpriteList, Camera2D
+from arcade.particles import Emitter, EmitMaintainCount, FadeParticle
 from pyglet.event import EVENT_HANDLE_STATE
 
 from sprites import V1, Slug, Worm, BrickWall, Bird
 from constants import PLAYER_SPEED
 from views.results_view import ResultsView
 
+SPARK_TEX = [
+    arcade.make_soft_circle_texture(8, arcade.color.PASTEL_YELLOW),
+    arcade.make_soft_circle_texture(8, arcade.color.PEACH),
+    arcade.make_soft_circle_texture(8, arcade.color.BABY_BLUE),
+    arcade.make_soft_circle_texture(8, arcade.color.ELECTRIC_CRIMSON),
+]
+
+
+def make_trail(attached_sprite, maintain=40):
+    emit = Emitter(
+        center_xy=(attached_sprite.center_x, attached_sprite.center_y),
+        emit_controller=EmitMaintainCount(maintain),
+        particle_factory=lambda e: FadeParticle(
+            filename_or_texture=random.choice(SPARK_TEX),
+            change_xy=arcade.math.rand_in_circle((0.0, 0.0), 1.6),
+            lifetime=random.uniform(0.35, 0.6),
+            start_alpha=220,
+            end_alpha=0,
+            scale=random.uniform(0.25, 0.4),
+        ),
+    )
+    emit._attached = attached_sprite
+    return emit
 
 class Game(arcade.View):
     def __init__(self, menu_view: arcade.View):
@@ -48,6 +72,7 @@ class Game(arcade.View):
         self.enemy_types = [Bird, Slug, Worm]
 
         self.world_camera = Camera2D()
+        self.emitters = []
 
     def get_config(self):
         with open('config.json') as file:
@@ -67,6 +92,8 @@ class Game(arcade.View):
         self.bullet_list.draw()
         self.enemies_list.draw()
         self.walls_list.draw()
+        for emitter in self.emitters:
+            emitter.draw()
 
     def on_update(self, delta_time: float) -> bool | None:
         """Обновляем все спрайты, врагам передаём позицию игрока"""
@@ -96,12 +123,18 @@ class Game(arcade.View):
                 self.kills += 1
 
         self.world_camera.position = self.get_player_coords()
+        emitters_copy = self.emitters.copy()
+        for emitter in emitters_copy:
+            emitter.update(delta_time)
+            if emitter.can_reap():
+                self.emitters.remove(emitter)
 
         if len(self.enemies_list) < 5:
             enemy_coords = self.get_random_points_on_circle(self.get_player_coords()[0], self.get_player_coords()[1], 1000)
             for i in enemy_coords:
                 enemy = random.choice(self.enemy_types)(*i)
                 self.enemies_list.append(enemy)
+                self.emitters.append(enemy.trail_emitter)
 
     def get_player_coords(self):
         return self.player.center_x, self.player.center_y
